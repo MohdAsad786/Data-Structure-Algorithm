ff7c8cc7c0258f0b54f534773c2a434f6b0358fc
MohdAsad786 <786asadmalik@gamil.com>
2024-08-31 22:10:16 +0530

    one program

---
+++ currencyExcahngeRate.java
@@ -0,0 +1,40 @@
+package JavaPrograms;
+
+import Dsa_Questions.ExchangeRate;
+
+import java.util.Scanner;
+
+public class currencyExcahngeRate {
+//    Input currency in rupees and output in USD.
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        System.out.println("Enter inr: ");
+        int inr=in.nextInt();
+        System.out.println("Enter $: ");
+
+        int usd=in.nextInt();
+        System.out.println("Enter 1  if you exhange into usd  Enter 0 if u exchange in inr: ");
+        int  convert =in.nextInt();
+        int ans = currencyConverter( inr, usd,convert );
+        System.out.println("your Exchange Rate: "+ans);
+    }
+
+     static int currencyConverter(  int inr, int usd,int convert) {
+        int ExchangeRate=0;
+        if(convert==1){
+//            int ExchangeRate=0;
+            ExchangeRate=inr/usd;
+            return ExchangeRate;
+
+        } else if (convert==0) {
+//            int ExchangeRate;
+            ExchangeRate =inr*usd;
+            return ExchangeRate;
+
+        }
+         return ExchangeRate;
+
+     }
+}
